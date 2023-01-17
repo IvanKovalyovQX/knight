d6726fe4477856a60041fccfa15b41c2d6159049
IvanKovalyovQX <ivan.kovalov@spsoft.com>
2023-01-17 07:02:01 +0200

    Update dataSet.ts

--- tests/utils/dataSet.ts
+++ tests/utils/dataSet.ts
@@ -7,16 +7,21 @@ const config: Config = {
 
 const messages = [
   'How are you doing?',
-  'Lets schedule a call',
+  'Can we schedule a call?',
   'I need to talk',
   'I have some questions',
   'I have some questions how to use specific functionality',
   'I feel myself bad',
+  'I’m so depressed. I just need someone to talk to.”',
+  'Let’s have a call and I suggest you some improvements that will help to other users” → “I have some ideas for improvements that might help other users!',
   'I feel myself very painful',
-  'I feel myself depressed',
+  'I’m feeling depressed.',
+  'Can you please help me invite my Caregiver?',
   'I need emergency right now',
-  'Can yo suggest me some articles related to my treatment',
-  'Can yo suggest me good cancer specialist',
+  'Can you please recommend some articles related to my cancer?',
+  'Can you recommend me some articles that will tell me what to expect with this new treatment I’m about to begin?',
+  'Can I get some advice on finding a good cancer specialist?',
+  'I want to invite my friend to this app and be in contact with hi, do we have this opportunity” → “I’d like to invite my friend to this app so we can talk here. Is that possible?'
 ]
 
 export const defaultMessages: string = uniqueNamesGenerator({
